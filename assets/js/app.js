6214ed7111118814b8932954006a679e17b6f49e
mahaUser2 <75357482+mahaUser2@users.noreply.github.com>
2023-03-06 21:05:04 +0100

    Added the base files and commented the app.js

---
+++ assets/js/app.js
@@ -0,0 +1,17 @@
+// Record this?
+
+// First - store a value using sessionStorage.setItem("key", "value")
+
+// Now get it and display it somewhere
+
+// Delete the sessionStorage and the code above and do the main task:
+
+// Get the form
+
+// Listen for submit event, prevent default, store the first+last name in sessionStorage
+
+// Get the .currentUser and store the value of the sessionStorage in there
+
+// Now try getting the sessionStorage from anotherfile.html
+
+// IF you have time - rework the data to store an array of data instead of a single object
